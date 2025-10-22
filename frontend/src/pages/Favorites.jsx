23ee1c4fee68872f9b7b87523de443ad7a431e9c
MWELU <nyolmorodney@gmail.com>
2025-10-22 06:43:24 +0300

    Initial commit - React movie explorer

---
+++ frontend/src/pages/Favorites.jsx
@@ -0,0 +1,30 @@
+import "../css/favorites.css";
+import { useMovieContext } from "../contexts/MovieContext";
+import MovieCard from "../components/MovieCard";
+
+function Favorites() {
+  const { favorites } = useMovieContext();
+
+ 
+  if (favorites.length === 0) {
+    return (
+      <div className="favorites-empty">
+        <h2>No Favorite Movies Yet</h2>
+        <p>Start adding movies to your favorites and they’ll appear here.</p>
+      </div>
+    );
+  }
+
+  return (
+    <div className="favorites">
+      <h2>Your Favorites ❤️</h2>
+      <div className="favorites-grid">
+        {favorites.map((movie) => (
+          <MovieCard movie={movie} key={movie.id} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Favorites;
